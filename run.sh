93d582bfc616bc31d573d465adc16fd422f5597f
Tomas Jonsson <jonsson@spotify.com>
2022-08-01 15:42:13 -0400

    update url for gitea

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-curl http://gitea.srv.linuxburken.se:3000/zooper/notifynyc/raw/branch/master/notify-nyc.py -o /notifynyc/notify-nyc.py
+curl https://gitea.linuxburken.se/zooper/notifynyc/raw/branch/master/notify-nyc.py -o /notifynyc/notify-nyc.py
 touch /log/log.txt
 python /notifynyc/notify-nyc.py
